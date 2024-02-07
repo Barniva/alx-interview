2e2f03e5d7ddb21679fa07c7128479f895246191
Barnabas Endashaw <123116734+Barniva@users.noreply.github.com>
2024-02-07 12:15:27 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''Minimum Operations python3 challenge'''
 
-
 def minOperations(n):
     '''calculates the fewest number of
     operations needed to result in exactly n H
@@ -14,46 +13,23 @@ def minOperations(n):
     counter = 0  # operations counter
 
     while pasted_chars < n:
-        # if did not copy anything yet
         if clipboard == 0:
-            # copyall
             clipboard = pasted_chars
-            # increment operations counter
             counter += 1
-
-        # if haven't pasted anything yet
         if pasted_chars == 1:
-            # paste
             pasted_chars += clipboard
-            # increment operations counter
             counter += 1
-            # continue to next loop
             continue
-
-        remaining = n - pasted_chars  # remaining chars to Paste
-        # check if impossible by checking if clipboard
-        # has more than needed to reach the number desired
-        # which also means num of chars in file is equal
-        # or more than in the clipboard.
-        # in both situations it's impossible to achieve n of chars
+        remaining = n - pasted_chars
         if remaining < clipboard:
             return 0
-
-        # if can't be devided
         if remaining % pasted_chars != 0:
-            # paste current clipboard
             pasted_chars += clipboard
-            # increment operations counter
             counter += 1
         else:
-            # copyall
             clipboard = pasted_chars
-            # paste
             pasted_chars += clipboard
-            # increment operations counter
             counter += 2
-
-    # if got the desired result
     if pasted_chars == n:
         return counter
     else:
