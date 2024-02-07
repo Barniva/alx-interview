eea232bf89bc0b8da79e8c6f024a91c14fae6ca6
Barniva <barnabasva1@gmail.com>
2024-02-07 11:05:42 +0300

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
