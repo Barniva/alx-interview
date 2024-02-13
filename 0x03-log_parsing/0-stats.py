a6ed101cd0988026440b8a975f6981ba1e010d2e
Barniva <barnabasva1@gmail.com>
2024-02-13 12:40:07 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,55 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+"""
+A script: Reads standard input line by line and computes metrics
+"""
 
-import sys
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-cache = {code: 0 for code in status_codes}
-total_size = 0
-counter = 0
+def parseLogs():
+    """
+    Reads logs from standard input and generates reports
+    Reports:
+        * Prints log size after reading every 10 lines & at KeyboardInterrupt
+    Raises:
+        KeyboardInterrupt (Exception): handles this exception and raises it
+    """
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) < 7:
-            continue
-        size = line_list[-1]
-        code = line_list[-2]
-        if code in cache:
-            cache[code] += 1
-            total_size += int(size)
-            counter += 1
 
-        if counter == 10:
-            print("File size: {}".format(total_size))
-            for code in sorted(cache.keys()):
-                if cache[code] > 0:
-                    print("{}: {}".format(code, cache[code]))
-            counter = 0
+def report(fileSize, statusCodes):
+    """
+    Prints generated report to standard output
+    Args:
+        fileSize (int): total log size after every 10 successfully read line
+        statusCodes (dict): dictionary of status codes and counts
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
-except KeyboardInterrupt:
-    pass
 
-finally:
-    print("File size: {}".format(total_size))
-    for code in sorted(cache.keys()):
-        if cache[code] > 0:
-            print("{}: {}".format(code, cache[code]))
+if __name__ == '__main__':
+    parseLogs()
