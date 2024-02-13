17135f1d3daaab9bab76c4acbf0885ece6990905
Barnabas Endashaw <123116734+Barniva@users.noreply.github.com>
2024-02-13 12:37:22 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 '''a script that reads stdin line by line and computes metrics'''
 
-
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+cache = {code: 0 for code in status_codes}
 total_size = 0
 counter = 0
 
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
+        if len(line_list) < 7:
+            continue
+        size = line_list[-1]
+        code = line_list[-2]
+        if code in cache:
+            cache[code] += 1
+            total_size += int(size)
             counter += 1
 
         if counter == 10:
+            print("File size: {}".format(total_size))
+            for code in sorted(cache.keys()):
+                if cache[code] > 0:
+                    print("{}: {}".format(code, cache[code]))
             counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
 
-except Exception as err:
+except KeyboardInterrupt:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print("File size: {}".format(total_size))
+    for code in sorted(cache.keys()):
+        if cache[code] > 0:
+            print("{}: {}".format(code, cache[code]))
