465daa77ac64ec7277979a6f0b12c4f360d55f06
Barniva <barnabasva1@gmail.com>
2024-03-08 17:47:55 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-'''Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet
-    a given amount total.
 '''
-import sys
-
-
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet
+a given amount total.
+'''
 def makeChange(coins, total):
     '''
     Return: fewest number of coins needed to meet total
@@ -14,16 +12,14 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
 
-    if table[total] == sys.maxsize:
-        return -1
-    return table[total]
+    # Initialize dp array with total+1 as we can't have more coins than total+1
+    dp = [total+1]*(total+1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total+1):
+            dp[amount] = min(dp[amount], dp[amount-coin] + 1)
+
+    return dp[total] if dp[total] != total+1 else -1
+
